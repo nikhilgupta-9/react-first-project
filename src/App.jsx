1ea0cfdca3d63adbafc197c44e852bdbca6023bc
Nikhil Gupta <iamnikhilgupta9@gmail.com>
2025-12-02 07:22:37 +0530

    this is third commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,16 @@ function App() {
        I'm using CI/CD pipeline and it deployed on versel
       </p>
       <p>this is learn with Next.js </p>
+
+      <div className='max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden'>
+        <img src="https://images.unsplash.com/photo-1587080413959-06b859fb107d?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvZmZlZSUyMGN1cHxlbnwwfHwwfHx8MA%3D%3D" alt="Sample Image" />
+        <div>
+          <h2>Card Title</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sit quos tempore debitis non aliquam deleniti, distinctio ab ducimus unde nam dolorem eveniet facere corrupti earum maxime ipsam quia voluptate!
+          Incidunt voluptate obcaecati atque itaque delectus eum laudantium autem optio ipsam asperiores tempora non, perferendis voluptates assumenda dolorum. Libero exercitationem saepe explicabo possimus, ex officiis voluptatum voluptatem iste quisquam magni.</p>
+          <button>Buy Now</button>
+        </div>
+      </div>
     </>
   )
 }
