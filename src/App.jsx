e1137e049faa7a057470f5783a3b55771a46b8f1
Nikhil Gupta <iamnikhilgupta9@gmail.com>
2025-12-02 06:45:07 +0530

    this is first commit on react project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>This is nikhil first react app </h1>
+      </div>
+      
+      <p className="read-the-docs">
+       I'm using CI/CD pipeline and it deployed on versel
+      </p>
+    </>
+  )
+}
+
+export default App
